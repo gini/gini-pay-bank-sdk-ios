ed4576827eb7c4763934cbd85a61aabaa5989ab0
Nadya Karaban <nadya@gini.net>
2021-05-03 13:30:05 +0200

    Bank example: updated pods and added input text fields (PIA-1411)

--- Example/Bank/AppDelegate.swift
+++ Example/Bank/AppDelegate.swift
@@ -6,21 +6,36 @@
 //
 
 import UIKit
+import GiniPayBank
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-    
+    var paymentRequestId: String = ""
+
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        _ = UIWindow(frame: UIScreen.main.bounds)
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
+                let viewController = storyboard.instantiateViewController(withIdentifier: "paymentViewController") as! PaymentViewController
+                let navigationController = UINavigationController.init(rootViewController: viewController)
+                self.window?.rootViewController = navigationController
 
+                self.window?.makeKeyAndVisible()
         return true
     }
     
     func application(_ app: UIApplication,
                      open url: URL,
                      options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-
+        receivePaymentRequestId(url: url) { result in
+            switch result {
+            case .success(let requestId):
+                self.paymentRequestId = requestId
+            case .failure(_):
+                print("failure")
+            }
+        }
         return true
     }
 
--- Example/Bank/ViewController.swift
+++ Example/Bank/ViewController.swift
@@ -6,14 +6,77 @@
 //
 
 import UIKit
+import GiniPayApiLib
+import GiniPayBank
 
-class ViewController: UIViewController {
+class PaymentViewController: UIViewController {
+    @IBOutlet weak var receipient: UITextField!
+    @IBOutlet weak var iban: UITextField!
+    @IBOutlet weak var amount: UITextField!
+    @IBOutlet weak var purpose: UITextField!
+    @IBOutlet weak var payButton: UIButton!
+    @IBOutlet weak var backToBusinessButton: UIButton!
+    
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    lazy var paymentRequestID: String = {
+        return appDelegate.paymentRequestId
+    }()
+    
+    lazy var bankSdk: GiniPayBank = {
+        let client = Client(id: "gini-mobile-test", secret: "wT4o4kXPAYtDknnOYwWf4w5s", domain: "gini.net")
+        let lib = GiniApiLib.Builder(client: client).build()
+        return GiniPayBank(with: lib)
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
+        fetchPaymentData()
     }
-
+    
+    func setupUI(){
+        payButton.isEnabled = false
+        payButton.addTarget(self, action: #selector(resolvePayment), for: .touchUpInside)
+        backToBusinessButton.isHidden = true
+        backToBusinessButton.addTarget(self, action: #selector(handleBackToBusiness), for: .touchUpInside)
+    }
+    
+    @objc func resolvePayment(){
+        let paymentInfo = PaymentInfo.init(recipient: receipient.text ?? "", iban: iban.text ?? "", bic: "", amount: amount.text ?? "", purpose: purpose.text ?? "")
+        
+        bankSdk.resolvePayment(paymentRequesId: paymentRequestID, paymentInfo: paymentInfo){ result in
+            switch result {
+            case .success(_):
+                self.backToBusinessButton.isHidden = false
+            case .failure(_):
+                break
+            }
+            
+        }
+    }
+    
+    @objc func handleBackToBusiness(){
+        bankSdk.returnBackToBusinessAppHandler()
+    }
+    
+    func fillOutTheData(paymentRequest: PaymentRequest){
+        receipient.text = paymentRequest.recipient
+        iban.text = paymentRequest.iban
+        amount.text = paymentRequest.amount
+        purpose.text = paymentRequest.purpose
+        payButton.isEnabled = true
+    }
+    
+    func fetchPaymentData(){
+        bankSdk.receivePaymentRequest(paymentRequestId: paymentRequestID) { result in
+            switch result {
+            case .success( let paymentRequest):
+                self.fillOutTheData(paymentRequest: paymentRequest)
+            case .failure(_): break
+                
+            }
+        }
+    }
 
 }
 
